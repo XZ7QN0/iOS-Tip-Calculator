47735f238214cd5b6264d8600eca3eec2b3ce20b
Jonathon <jchenvert1@gmail.com>
2019-11-07 12:11:18 -0800

    Initial Tip Calculator creation

--- tipMeOut/ViewController.swift
+++ tipMeOut/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalBillLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Dismisses keyboard by tapping anywhere on the screen
+    @IBAction func onTap(_ sender: Any) {
+        print("Keyboard has been dismissed by user.")
+        
+        view.endEditing(true)
+    }
+    
+    // TODO: Create a custom tip option with
+    // text field that shows up only when pressed on UISegmentControl
+    // TODO: Create initial launch screen that shows and describes how the app works (1st time only)
+    /**
+            Calculates the tip and the total balance from the user's bill
+     - Important:
+     This code has not gone through any QA.
+     
+     - Version:
+     0.1
+     */
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the intial bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate the tip value and total value
+        let tipPercentages = [0.1, 0.15, 0.18, 0.2, 0.23, 0.25]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalBillLabel.text = String(format: "$%.2f", total)
+    }
 }
 
